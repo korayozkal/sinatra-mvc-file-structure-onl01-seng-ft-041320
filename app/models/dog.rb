ef143e36027defb493f117e40850605383c0d627
Koray Ozkal <koryozkal@gmail.com>
2020-06-04 04:24:23 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,16 @@
 class Dog 
 attr_accessor :name, :breed, :age 
-@@all=[]
+@@all = []
 def initialize 
   @name = name
   @breed = breed
   @age = age 
   @all << self
+end
+
+def self.all
+  @@all
 end 
 end 
+
+
